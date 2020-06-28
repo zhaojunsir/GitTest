7d745b5d23109c955d44ae714970717e3bb8124a
zhaojunsir <zhaojunsir@163.com>
2020-06-28 14:39:43 +0800

    merge origin/master分支代码

--- src/gitTest/World.java
+++ src/gitTest/World.java
@@ -3,7 +3,6 @@ package gitTest;
 public class World {
 
 	public static void main(String[] args) {
-
 		System.out.println("hello china");
 		System.out.println("第二次提交");
 		System.out.println("本地第一次");
