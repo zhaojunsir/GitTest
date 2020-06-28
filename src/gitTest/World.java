7dd31ab5e04f9a4f0677d993012d1207967fa930
zhaojunsir <zhaojunsir@163.com>
2020-06-28 15:52:58 +0800

    gitTest项目World.java修改

--- src/gitTest/World.java
+++ src/gitTest/World.java
@@ -7,8 +7,8 @@ public class World {
 		System.out.println("第二次提交");
 		System.out.println("本地第一次");
 		System.out.println("该功能已经完成，准备提交到remote git repository");
-                System.out.println("其他项目组成员新增代码");
-		System.out.println("其他项目组成员新增功能22");
+        System.out.println("其他项目组成员新增代码");
+		System.out.println("其他项目组成员新增功能2244556677");
 	}
 
 }
