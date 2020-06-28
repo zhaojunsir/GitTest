b39f783a6994f2319d0961b1d16fecd6c4728653
zhaojunsir <zhaojunsir@163.com>
2020-06-28 09:14:25 +0800

    iss53提交代码

--- src/gitTest/Hello.java
+++ src/gitTest/Hello.java
@@ -6,6 +6,7 @@ public class Hello {
 		System.out.println("Hello World!!");
 		System.out.println("master分支上Hello.java提交");
 		System.out.println("现在是dev分支上进行开发工作");
+		System.out.println("iss53分支新增代码");
 	}
 
 }
